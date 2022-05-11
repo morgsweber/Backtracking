072f438fa6d1f448391f68a5aceb52089e8593c2
Morgana Weber <50406261+morgsweber@users.noreply.github.com>
2022-05-11 09:24:23 -0300

    success :)

--- Main.java
+++ Main.java
@@ -5,139 +5,63 @@
     Trabalho Prático 3
 */
 
-import java.util.ArrayList;
-
 public class Main {
-    public static int SUML1;
-    public static int SUML2;
-    public static int SUML3;
-
-    public static int SUMD1;
-    public static int SUMD2;
-    public static int SUMD3;
-
-    public static int SUMD4;
-    public static int SUMD5;
-    public static int SUMD6;
+    public static int[] used = new int[15];
 
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
-        for (int i = 2; i <= 16; i++) {
-            if(i != 8){
-                numbers.add(i); //create the numbers list without the 8
-            }    
-        }
-        System.out.println(8); //put the 8 on screen
-
-        //add 8 to sums 
-        SUMD1 += 8;
-        SUMD4 += 8;
-        FillRec(numbers, 2);
+        int[] numbers = {2,3,4,5,6,7,9,10,11,12,13,14,15,16};
+        used[0] = 8;
+        FillRec(1, used.length, numbers, 0,0,0,0,0,0,0,0,0);
     }
 
-    public static void FillRec(ArrayList<Integer> numbers, int node) {
-        Integer n = numbers.get(0);
-        numbers.remove(n);
+    public static void FillRec(int node, int size, int[] numbers, int suml1, int suml2, int suml3, int sumd1, int sumd2, int sumd3, int sumd4, int sumd5, int sumd6) {
+        if(node >= size){
+            for(int i = 0; i<size; i++){
+                System.out.print(used[i] + " ");
+            }
+            System.out.println();
+            return;
+        }   
 
-        if(node == 1){
-            FillRec(numbers, node+1);
-            numbers.add(n);
-        }
+        for(int i=0; i<size-1; i++){
+            if(numbers[i] == 0){ continue; }
+            int n = numbers[i];  
 
-        if(numbers.size() == 0){
-            FillRec(numbers, node-1);
-            numbers.add(n);
-        }
-        System.out.print("node: " + node + " " + n + " ");
+            if(node == 5){ 
+                suml1 = used[3] + used[4] + n;
+            }
+            else if(node == 9){
+                suml2 = used[6] + used[7] + used[8] + n;
+                if(suml2 != suml1){ continue; }
+            }
+            else if(node == 10){
+                sumd1 = used[0] + used[1] + used[3] + used[6] + n;
+                if(sumd1 != suml1){ continue; }
+            }
+            else if(node == 11){
+                sumd2 = used[2] + used[4] + used[7] + n;
+                if(sumd2 != suml1){ continue; }
+            }
+            else if(node == 12){
+                sumd3 = used[5] + used[8] + n;
+                sumd6 = used[3] + used[7] + n;
+                if(sumd3 != suml1 || sumd6 != suml1){ continue; }
+            }
+            else if(node == 13){
+                sumd5 = used[1] + used[4] + used[8] + n;
+                if(sumd5 != suml1){ continue; }
+            }
+            else if(node == 14){
+                suml3 = used[10] + used[11] + used[12] + used[13] + n;
+                sumd4 = used[0] + used[2] + used[5] + used[9] + n;   
+                if(sumd4 != suml1 || suml3 != suml1){ continue; }
+            }
 
-        //do the sums 
-        if (node == 2) {
-            SUMD1 += n;
-            SUMD5 += n;
-        } else if (node == 3) {
-            System.out.println();
-            SUMD4 += n;
-            SUMD2 += n;
-        } else if (node == 4) {
-            SUMD1 += n;
-            SUML1 += n;
-            SUMD6 += n;
-        } else if (node == 5) {
-            SUMD5 += n;
-            SUML1 += n;
-            SUMD2 += n;
-        } else if (node == 6) { // value to be found: SUML1
-            System.out.println();
-            SUMD3 += n;
-            SUML1 += n;
-            SUMD4 += n;
-        } else if (node == 7) {
-            SUMD1 += n;
-            SUML2 += n;
-        } else if (node == 8) {
-            SUMD2 += n;
-            SUML2 += n;
-            SUMD6 += n;
-        } else if (node == 9) {
-            SUMD3 += n;
-            SUML2 += n;
-            SUMD5 += n;
-        } else if (node == 10) { //compare with SUML2
-            System.out.println();
-            SUML2 += n;
-            SUMD4 += n;
-            if(SUML2 != SUML1){
-                SUML2 -= n;
-                SUMD4 -= n;
-            }
-        } else if (node == 11) { //compare with SUMD1
-            SUMD1 += n;
-            if(SUMD1 != SUML1){
-                SUMD1 -= n;
-                FillRec(numbers, node);
-                numbers.add(n);
-            }
-            
-            SUML3 += n;
-        } else if (node == 12) { //compare with SUMD2
-            SUMD2 += n;
-            if(SUMD2 != SUML1){
-                SUMD2 -= n;
-                FillRec(numbers, node);
-                numbers.add(n);
-            }
-            SUML3 += n;
-        } else if (node == 13) { //compare with SUMD6 and SUMD3
-            SUMD6 += n;
-            SUMD3 += n;
-            if(SUMD6 != SUML1 || SUMD3 != SUML1){
-                SUMD6 -= n;
-                SUMD3 -= n;
-                FillRec(numbers, node);
-                numbers.add(n);
-            }
-            SUML3 += n;
-        } else if (node == 14) { //compare with SUMD5
-            SUMD5 += n;
-            if(SUMD5 != SUML1){
-                SUMD5 -= n;
-                FillRec(numbers, node); 
-                numbers.add(n);
-            }
-            SUML3 += n;
-        } else if (node == 15) { //compare with SUML3 and SUMD4
-            System.out.println();
-            SUMD4 += n;
-            SUML3 += n;
-            if(SUMD4 != SUML1 || SUML3 != SUML1){
-                SUMD4 -= n;
-                SUML3 -= n;
-                FillRec(numbers, node);
-                numbers.add(n);
-            }
-        }
-        FillRec(numbers, node+1);
-        numbers.add(n);
+            used[node] = n;
+            numbers[i] = 0;
+            FillRec(node+1, size, numbers, suml1, suml2, suml3, sumd1, sumd2, sumd3, sumd4, sumd5, sumd6);
+            numbers[i] = used[node];
+        }        
     }
 }
 
